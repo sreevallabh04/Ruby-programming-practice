a2b66c7662ca2ede640a9874b6f825dca6aeb415
sreevallabh <srivallabhkakarala@gmail.com>
2025-07-14 12:42:23 +0530

    taking input and printing

--- beginner_ruby.rb
+++ beginner_ruby.rb
@@ -1,8 +1,6 @@
-puts "Hello world"
-name = "Sreevallabh"
-puts "Welcome is #{name}, How are you"
-age = 20
-puts "I am #{age} years old."
-p "The age an be incremented #{age + 1} years old."
-x,y=5,8
-p "sum of x and y is #{x+y}"
+puts "Enter your name: "
+name= gets.chomp
+puts "Your name is #{name}"
+puts "Enter your age: "
+age = gets.chomp.to_i
+puts "Welcome #{name}, your age is #{age}"
