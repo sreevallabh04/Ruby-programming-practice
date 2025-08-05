55ef056dce9ed669ab37c6a413348fc70392bd5a
sreevallabh <srivallabhkakarala@gmail.com>
2025-08-05 16:08:40 +0530

    shift and unshift

---
+++ 5thaugust.rb
@@ -0,0 +1,17 @@
+#5th August Tuesday 2025
+# shift and unshift Arrays
+
+arr=[1,2,3,4,5,6,7,8,9,10]
+p arr
+# arr.shift
+# p arr
+# extract=arr.shift
+# p extract
+# p arr.shift(2)
+
+arr.unshift(100)
+p arr
+arr.unshift(100,200)
+p arr
+arr.unshift(100,200,300)
+p arr
