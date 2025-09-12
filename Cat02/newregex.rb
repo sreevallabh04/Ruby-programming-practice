6899764fa79ba69b9fcefdd8d71016986a8171ca
sreevallabh <srivallabhkakarala@gmail.com>
2025-09-12 14:46:19 +0530

    final push

--- Cat02/newregex.rb
+++ Cat02/newregex.rb
@@ -12,3 +12,29 @@ p paragraph.scan(/\D/)
 p paragraph.scan(/\S/)
 p paragraph.scan(/\s/).length
 p paragraph.scan(/\S/)
+
+poem= "and roses are red, not always"
+p poem.scan(/\A\d+/)
+
+p poem.scan(/ays\z/)
+
+
+#exclude
+
+sales= "I bought 9 apples, 25 bananas and 4 oranges at the store"
+puts sales.scan(/[^aeiouAEIOU]/)
+puts sales.scan(/[^aeiou]/)
+puts sales.scan(/[^a-z]/)
+
+#sub and gsub
+puts "wordplay".sub("w","sw")
+puts "wordplay".sub("word","sword")
+word = "aspirin"
+puts word.sub("in","ing")
+
+#gsub
+
+puts "an apple".gsub("a","an")
+
+puts "5555551234".gsub("5","6")
+puts "(555-555-1234".gsub("-","")
